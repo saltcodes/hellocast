5a2c754510f86b956a934aecdc8f9a12d96ae903
Johnson Osei Yeboah <johnyoat@gmail.com>
2020-07-03 16:23:40 +0000

    initial commit

---
+++ app/src/main/java/me/johnyoat/hellocast/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.johnyoat.hellocast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
