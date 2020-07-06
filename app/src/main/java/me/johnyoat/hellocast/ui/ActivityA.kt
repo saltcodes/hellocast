6e42992d1a0d5925d75692cf30017febf62ee242
Johnson Osei Yeboah <johnyoat@gmail.com>
2020-07-06 23:49:26 +0000

    test activity transition

---
+++ app/src/main/java/me/johnyoat/hellocast/ui/ActivityA.kt
@@ -0,0 +1,12 @@
+package me.johnyoat.hellocast.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.johnyoat.hellocast.R
+
+class ActivityA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_a)
+    }
+}
