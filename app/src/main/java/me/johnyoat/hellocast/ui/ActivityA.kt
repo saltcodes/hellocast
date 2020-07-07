09ef3b40d47cde503962d9155f9cc9d6e3d27f8a
Johnson Osei Yeboah <johnyoat@gmail.com>
2020-07-07 00:10:42 +0000

    ready for Material Transform Animation

--- app/src/main/java/me/johnyoat/hellocast/ui/ActivityA.kt
+++ app/src/main/java/me/johnyoat/hellocast/ui/ActivityA.kt
@@ -1,12 +1,25 @@
 package me.johnyoat.hellocast.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityOptionsCompat
+import com.google.android.material.transition.platform.MaterialContainerTransformSharedElementCallback
+import kotlinx.android.synthetic.main.activity_a.*
 import me.johnyoat.hellocast.R
 
 class ActivityA : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        setExitSharedElementCallback(MaterialContainerTransformSharedElementCallback())
+        window.sharedElementsUseOverlay = false
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_a)
+
+        albumCard.setOnClickListener {
+            val intent = Intent(this,ActivityB::class.java)
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this,it,"album_transition")
+            startActivity(intent,options.toBundle())
+        }
     }
 }
--- app/src/main/java/me/johnyoat/hellocast/ui/ActivityB.kt
+++ app/src/main/java/me/johnyoat/hellocast/ui/ActivityB.kt
@@ -2,10 +2,27 @@ package me.johnyoat.hellocast.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.transition.platform.MaterialContainerTransform
+import com.google.android.material.transition.platform.MaterialContainerTransformSharedElementCallback
 import me.johnyoat.hellocast.R
 
 class ActivityB : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        findViewById<View>(android.R.id.content).transitionName = "album_transition"
+
+        setExitSharedElementCallback(MaterialContainerTransformSharedElementCallback())
+
+        window.sharedElementEnterTransition = MaterialContainerTransform().apply {
+            addTarget(android.R.id.content)
+            duration = 300L
+        }
+
+        window.sharedElementExitTransition = MaterialContainerTransform().apply {
+            addTarget(android.R.id.content)
+            duration = 350L
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b)
     }
