447b407694fc634555d4959185cc9d3a4452034c
Johnson Osei Yeboah <johnyoat@gmail.com>
2020-07-07 00:39:39 +0000

    material trnasform for fragment

---
+++ app/src/main/java/me/johnyoat/hellocast/ui/FragmentA.kt
@@ -0,0 +1,25 @@
+package me.johnyoat.hellocast.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import me.johnyoat.hellocast.R
+
+class FragmentA : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_a, container, false)
+    }
+
+
+}
