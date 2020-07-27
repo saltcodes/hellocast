6d95e00e33b97e259110638259017d205942b8d4
Johnson Osei Yeboah <johnyoat@gmail.com>
2020-07-27 23:06:02 +0000

    search view redesigned

--- app/src/main/java/me/johnyoat/hellocast/ui/search/SearchActivity.kt
+++ app/src/main/java/me/johnyoat/hellocast/ui/search/SearchActivity.kt
@@ -2,15 +2,31 @@ package me.johnyoat.hellocast.ui.search
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
+import androidx.core.widget.addTextChangedListener
 import me.johnyoat.hellocast.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
 
-    private var  _binding:ActivitySearchBinding? = null
+    private var _binding: ActivitySearchBinding? = null
     private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.clearBtn.isGone = true
+
+        binding.clearBtn.setOnClickListener {
+            binding.searchText.setText("")
+        }
+
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
+
+        binding.searchText.addTextChangedListener {
+            binding.clearBtn.isGone = it?.length!! <= 3
+        }
     }
 }
